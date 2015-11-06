b5af6d6566540758c64b46a2a36f0773c1d97929
JFitzMan <fitzpatrickj1@email.arizona.edu>
2015-11-05 18:33:21 -0700

    Changed the flow so that clock driver quits when it's zapped, and start3 joins with it, like the spec says

--- phase4.c
+++ phase4.c
@@ -14,10 +14,12 @@ int procTable_mutex;
 //process table
 struct procSlot procTable[MAXPROC];
 
-int debugflag4 = 1;
+int debugflag4 = 0;
 
 static int ClockDriver(char *);
 static int DiskDriver(char *);
+
+static procPtr SleepList;
 
 void
 start3(void)
@@ -49,6 +51,9 @@ start3(void)
     //initialize phase 4 process table
     initializeProcTable();
 
+    //initialize the SleepList
+    SleepList = NULL;
+
     /*
      * Create clock device driver 
      * I am assuming a semaphore here for coordination.  A mailbox can
@@ -60,8 +65,6 @@ start3(void)
 	   USLOSS_Console("start3(): Can't create clock driver\n");
 	   USLOSS_Halt(1);
     }
-    if (debugflag4)
-        USLOSS_Console("start3(): Started clock driver process, about to wait for it\n");
     /*
      * Wait for the clock driver to start. The idea is that ClockDriver
      * will V the semaphore "semRunning" once it is running.
@@ -69,7 +72,7 @@ start3(void)
 
     sempReal(semRunning);
     if (debugflag4)
-        USLOSS_Console("start3(): synched up with clock driver\n");
+        USLOSS_Console("start3(): Started clock driver and synched with it via semRunning\n");
 
     /*
      * Create the disk device drivers here.  You may need to increase
@@ -110,6 +113,7 @@ start3(void)
      * Zap the device drivers
      */
     zap(clockPID);  // clock driver
+    join(&status);
 
     // eventually, at the end:
     quit(0);
@@ -123,23 +127,30 @@ ClockDriver(char *arg)
     int status;
 
     // Let the parent know we are running and enable interrupts.
-    if (debugflag4)
-        USLOSS_Console("ClockDriver(): Waking up start3 with semvReal()\n");
     semvReal(semRunning);
+    if (debugflag4)
+        USLOSS_Console("ClockDriver(): Starting\n");
     USLOSS_PsrSet(USLOSS_PsrGet() | USLOSS_PSR_CURRENT_INT);
 
     // Infinite loop until we are zap'd
     while(! isZapped()) {
-	result = waitDevice(USLOSS_CLOCK_DEV, 0, &status);
-	if (result != 0) {
-	    return 0;
-	}
-	/*
-	 * Compute the current time and wake up any processes
-	 * whose time has come.
-	 */
+        //send to wait device mailbox
+	    result = waitDevice(USLOSS_CLOCK_DEV, 0, &status);
+        if (debugflag4)
+            USLOSS_Console("ClockDriver(): checking for procs to wake up\n");
+	    if (result != 0) {
+	       return 0;
+	    }
+
+        int curTime = USLOSS_Clock();
+        //wakeup loop
+        while(SleepList != NULL && SleepList->timeToWakeUp < curTime){
+            wakeUpNextProc();
+        }
+
     }
-    return 0;
+    //been zapped, quitting time
+    quit(0);
 }
 
 static int
@@ -152,10 +163,46 @@ DiskDriver(char *arg)
 void
 sleep(systemArgs *args){
 
-    int seconds = args->arg1;
+    int seconds = (int)args->arg1;
+    int me = getpid();
 
     if (debugflag4)
-        USLOSS_Console("sleep(): got %d seconds from Sleep\n", seconds);
+        USLOSS_Console("sleep(): got %d seconds from Sleep. Goodnight world\n", seconds);
+
+    //ensure the process table is set up for this process
+    if (procTable[me].pid == -1){
+        setUpSleepSlot(me);
+    }
+    //call sleepReal
+    int toReturn = sleepReal(seconds);
+
+    if (toReturn  == -1){
+        USLOSS_Console("sleep(): There was a problem, sleepReal returned -1\n");
+    }
+    args->arg1 = 0;
+    //set back to user mode before retuning to system call Sleep
+    setToUserMode();
+}
+
+//adds process to sleep list and blocks
+int 
+sleepReal(int seconds){
+
+    int curTime = USLOSS_Clock();
+    int me = getpid();
+    //update values in the process table
+    procTable[me].timeToWakeUp  = curTime + seconds*1000000;
+    //add to sleepList
+    addToSleepList(&procTable[me]);
+    //block on private mailbox
+    MboxReceive(procTable[me].privateMbox, NULL, 0);
+    //if the time it was asleep is less than it was supposed to, -1
+    if (debugflag4)
+        USLOSS_Console("sleepReal(): waking up from sleep\n");
+    if (procTable[me].timeToWakeUp < curTime + seconds*1000000)
+        return -1;
+    return 0;
+
 }
 
 //utility function, will halt if kernel mode is called
@@ -176,21 +223,20 @@ inKernelMode(char *procName)
 //removed it from start3 because it was very long
 void
 initializeProcTable(){
+
+    MboxSend(procTable_mutex, NULL, 0);
     //inititalize process table
     for(int i = 0; i < MAXPROC; i++){
         procTable[i].pid = -1;
-        procTable[i].parentPid = -1;
-        procTable[i].nextChild = NULL;
-        procTable[i].nextSib = NULL;
+        procTable[i].nextProc = NULL;
         procTable[i].priority = -1;
-        procTable[i].start_func = NULL;
         procTable[i].name = NULL;
-        procTable[i].arg = NULL;
-        procTable[i].stack_size = -1;
         procTable[i].privateMbox = -1;
         procTable[i].termCode = -1;
         procTable[i].status = -1;
+        procTable[i].timeToWakeUp = -1;
     }
+
     //Manually add in entries for sentinel, start1, and start2
     procTable[1].name = "sentinel";
     procTable[1].priority = 6;
@@ -206,17 +252,90 @@ initializeProcTable(){
     procTable[3].status = JOIN_BLOCKED;
 
     //get start3 set in in proc table
-
     procTable[getpid()].pid = getpid();
-    procTable[getpid()].parentPid = 3;  //start2 PID
     procTable[getpid()].name = "start2";
     procTable[getpid()].status = READY;
-    procTable[getpid()].stack_size = USLOSS_MIN_STACK;
     procTable[getpid()].priority = 1;
-    procTable[getpid()].nextChild = NULL;
-    procTable[getpid()].nextSib = NULL;
-    procTable[getpid()].privateMbox = MboxCreate(0,sizeof(int[2]));
-    procTable[getpid()].termCode = -1;
+    procTable[getpid()].privateMbox = MboxCreate(0,50);
 
-} //end initializeProcTable
+    MboxReceive(procTable_mutex, NULL, 0);
+
+    if (debugflag4)
+        USLOSS_Console("initializeProcTable(): Completed\n");
+
+}
+
+//get a process table slot ready for bed
+void setUpSleepSlot(int pid){
+    procTable[pid].pid = pid;
+    procTable[pid].status = SLEEPING;
+    procTable[pid].privateMbox = MboxCreate(0,50);
+}
+
+//wakes up the proc at the front of the SleepList, and updates the SleepList
+void wakeUpNextProc(){
+    //grab the proc to wake up
+    procPtr toWake = SleepList;
+    if (debugflag4)
+        USLOSS_Console("wakeUpNextProc(): waking up process %d\n", toWake->pid);
+    //update the sleep list
+    if (SleepList->nextProc == NULL)
+        SleepList = NULL;
+    else
+        SleepList = SleepList->nextProc;
+
+    //update the procs table
+    toWake->status = READY;
+    toWake->nextProc = NULL;
+    //wake up by sending to private mbox
+    MboxSend(toWake->privateMbox, NULL, 0);
+}
+
+void setToUserMode(){
+    unsigned int psr = USLOSS_PsrGet();
+    psr = psr >> 1;
+    psr = psr << 1;
+    USLOSS_PsrSet(psr);
+}
+
+//adds to sleep list, and maintains the ordered queue
+void addToSleepList(procPtr toAdd){
+
+    //list is empty, toAdd is the front
+    if(SleepList == NULL){
+        SleepList = toAdd;
+    }
+    //toAdd has the shortest nap on the list
+    else if (toAdd->timeToWakeUp < SleepList->timeToWakeUp){
+        toAdd->nextProc = SleepList;
+        SleepList = toAdd;
+    }
+    //scann until toAdd fits in
+    else{
+        procPtr prev = NULL;
+        procPtr cur;
+        int added = 0;
+        for (cur = SleepList; cur != NULL; cur = cur->nextProc){
+            if (cur->timeToWakeUp > toAdd->timeToWakeUp){
+                prev->nextProc =toAdd;
+                toAdd->nextProc = cur;
+                added = 1;
+                break;
+            }
+            prev = cur;
+        }//end for
+
+        //check to see if it was  added. If not, it goes at the end
+        if(!added){
+            cur = SleepList;
+            while (cur->nextProc != NULL){
+                cur = cur->nextProc;
+            }
+            cur->nextProc = toAdd;
+        }
+    }
+
+    if (debugflag4)
+        USLOSS_Console("addToSleepList(): Process %d added to SleepList, timeToWakeUp: %d\n",toAdd->pid, toAdd->timeToWakeUp);
+}
 
